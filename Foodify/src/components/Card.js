feeaa24c5f9876149b9fa471fdf6d81fb3b3a2d9
Kshitiz <72270154+Kshitiz0302@users.noreply.github.com>
2024-06-27 02:25:03 +0530

    Update Card.js

--- Foodify/src/components/Card.js
+++ Foodify/src/components/Card.js
@@ -2,7 +2,7 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatchCart, useCart } from './ContextReducer'
-// import { Dropdown, DropdownButton } from 'react-bootstrap';
+
 export default function Card(props) {
   let data = useCart();
 
@@ -10,15 +10,12 @@ export default function Card(props) {
   const [qty, setQty] = useState(1)
   const [size, setSize] = useState("")
   const priceRef = useRef();
-  // const [btnEnable, setBtnEnable] = useState(false);
-  // let totval = 0
-  // let price = Object.values(options).map((value) => {
-  //   return parseInt(value, 10);
-  // });
+ 
+  
   let options = props.options;
   let priceOptions = Object.keys(options);
   let foodItem = props.item;
-  const dispatch = useDispatchCart();
+  const dispatch = useDispatchCart(); 
   const handleClick = () => {
     if (!localStorage.getItem("token")) {
       navigate("/login")
@@ -40,8 +37,8 @@ export default function Card(props) {
       }
     }
     console.log(food)
-    console.log(new Date())
-    if (food !== []) {
+    console.log(new Date()) 
+    if (food.length !== 0) {
       if (food.size === size) {
         await dispatch({ type: "UPDATE", id: foodItem._id, price: finalPrice, qty: qty })
         return
@@ -57,21 +54,14 @@ export default function Card(props) {
     await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size })
 
 
-    // setBtnEnable(true)
-
   }
 
   useEffect(() => {
     setSize(priceRef.current.value)
   }, [])
 
-  // useEffect(()=>{
-  // checkBtn();
-  //   },[data])
 
-  let finalPrice = qty * parseInt(options[size]);   //This is where Price is changing
-  // totval += finalPrice;
-  // console.log(totval)
+  let finalPrice = qty * parseInt(options[size]);  
   return (
     <div>
 
@@ -81,13 +71,13 @@ export default function Card(props) {
           <h5 className="card-title">{props.foodName}</h5>
           {/* <p className="card-text">This is some random text. This is description.</p> */}
           <div className='container w-100 p-0' style={{ height: "38px" }}>
-            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} onClick={handleClick} onChange={handleQty}>
+            <select className="m-2 h-80 w-20 bg-success text-white rounded" style={{ select: "#FF0000" }} onClick={handleClick} onChange={handleQty}>
               {Array.from(Array(6), (e, i) => {
                 return (
                   <option key={i + 1} value={i + 1}>{i + 1}</option>)
               })}
             </select>
-            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} ref={priceRef} onClick={handleClick} onChange={handleOptions}>
+            <select className="m-2 h-80 w-20 bg-success text-white rounded" style={{ select: "#FF0000" }} ref={priceRef} onClick={handleClick} onChange={handleOptions}>
               {priceOptions.map((i) => {
                 return <option key={i} value={i}>{i}</option>
               })}
@@ -98,7 +88,7 @@ export default function Card(props) {
           </div>
           <hr></hr>
           <button className={`btn btn-success justify-center ms-2 `} onClick={handleAddToCart}>Add to Cart</button>
-          {/* <button className={`btn btn-danger justify-center ms-2 ${btnEnable ? "" : "disabled"}`} onClick={handleRemoveCart}>Remove</button> */}
+          
         </div>
       </div>
     </div>
