8ca2261cc6d8dc1c243e64ee49f72646d03eb6cc
Kshitiz <72270154+Kshitiz0302@users.noreply.github.com>
2024-06-28 00:26:55 +0530

    Update Home.js

--- Foodify/src/screens/Home.js
+++ Foodify/src/screens/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [foodItems, setFoodItems] = useState([])
   const [search, setSearch] = useState('')
   const loadFoodItems = async () => {
-    let response = await fetch("http://localhost:5000/api/auth/foodData", {
+    let response = await fetch("http://https://foodify-backend-4l21.onrender.com/api/auth/foodData", {
       
       
       method: 'POST',
